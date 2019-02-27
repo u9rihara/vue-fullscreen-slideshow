5f270804c52911944a4ebad62758b9090417d235
Yu Kurihara <kurihara@uzu-lab.co.jp>
2019-02-27 17:35:32 +0900

    first commit

---
+++ src/Slides.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ greeting }} World!</p>
+</template>
+
+<script>
+  module.exports = {
+    data: () => {
+      return {
+        greeting: 'Hello'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  $text-color: red;
+  p {
+    color: $text-color;
+  }
+</style>
---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import '../sass/style.scss';
+import Vue from 'vue';
+import Slides from '../Slides.vue';
+
+new Vue({
+  el: '#app',
+  components: {
+    Slides,
+  },
+  template: '<slides></slides>',
+  data: {
+  },
+  created: function() {
+  },
+  methods: {
+
+  }
+});
